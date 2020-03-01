827458e3bc538b3dcc9298e5de13fce3d1f30c46
saladNights <the.withered.fern@gmail.com>
2020-03-01 10:33:45 +0300

    jwt login

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,19 +1,26 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql';
-import { hash } from 'bcryptjs';
+import {Resolver, Query, Mutation, Arg, ObjectType, Field} from 'type-graphql';
+import { hash, compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import {User} from './entity/User';
+
+@ObjectType()
+class LoginResponse {
+	@Field()
+	accessToken: string
+}
 
 @Resolver()
 export class UserResolver {
-	@Query(() => String)
-	hello() {
-		return 'hello!';
+	@Query(() => [User])
+	users() {
+		return User.find();
 	}
 
 	@Mutation(() => Boolean)
 	async register(
 		@Arg('email') email: string,
 		@Arg('password') password: string,
-	) {
+	): Promise<boolean> {
 		const hashedPassword = await hash(password, 12);
 
 		try {
@@ -27,6 +34,34 @@ export class UserResolver {
 			console.error(err);
 
 			return false;
+		}
+	}
+
+	@Mutation(() => LoginResponse)
+	async login(
+		@Arg('email') email: string,
+		@Arg('password') password: string,
+	): Promise<LoginResponse> {
+		const user = await User.findOne({ where: { email } });
+
+		if (!user) {
+			throw new Error ('Login Error');
+		}
+
+		const valid = await compare(password, user.password);
+
+		if (!valid) {
+			throw new Error ('Login Error');
+		}
+
+		// success
+
+		return {
+			accessToken: sign(
+				{ userId: user.id },
+				'RaKx8jUKcV9nJkrRezM2',
+				{ expiresIn: '15m'}
+				)
 		}
 	}
 }
